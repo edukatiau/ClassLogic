be6ed8ab07f1053a729586a09358bf2ed7ac4e3f
edukatiau <70084379+edukatiau@users.noreply.github.com>
2021-09-10 23:09:53 -0300

    Delete Aula1Python.py

--- Aula1Python.py
+++
@@ -1,9 +0,0 @@
-nota1 = int(input(Insira a nota 1: ))
-nota2 = int(input(Insira a nota 2: ))
-
-media = (nota1 + nota2) / 2
-
-if(media >= 7):
-    print("Aprovado!")
-
-print(f"Sua média é: {media}")
